534860b30ae74df6476fa6bb442d0613693e1d87
Pablo Dias <pabl_dias@icloud.com>
2026-03-23 10:22:25 -0300

    Implementação de Calculadora Modular em C
    Desenvolvimento de uma calculadora funcional utilizando funções para operações matemáticas, estrutura de decisão switch-case e tratamento de erros (validação de divisão por zero). Projeto acadêmico para a disciplina de Estrutura de Dados.

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-double somar(double a, double b);
-double subtrair(double a, double b);
-double multiplicar(double a, double b);
-double dividir(double a, double b);
+double somar(double a, double b) {return a + b;}
+double subtrair(double a, double b) {return a - b;}
+double multiplicar(double a, double b) {return a * b;}
+double dividir(double a, double b) {return a / b;}
 
 int main() {
     char operacao;
@@ -13,5 +13,24 @@ int main() {
     scanf(" %c", &operacao);
 
     printf("Digite o primeiro número: ");
-    scanf("%lf lf", %n1, %n2);
+    scanf("%lf", &num1);
+    printf("Digite o segundo número: ");
+    scanf("%lf", &num2);
+
+    switch (operacao) {
+        case '+': resultado = somar(num1, num2); 
+        break;
+        case '-': resultado = subtrair(num1, num2); 
+        break;
+        case '*': resultado = multiplicar(num1, num2); 
+        break;
+        case '/': if(num2 !=0) { 
+            resultado = dividir(num1, num2);}
+        else {printf("Erro: Divisao por zero!\n"); return 1;} 
+        break;
+        default: printf("Operador invalido!\n"); return 1;
+    }
+
+    printf("Resultado: %.2lf\n", resultado);
+    return 0;
 }
