14727caee8c3731e411afad36c45d1a547adfbc6
Pablo Dias <pabl_dias@icloud.com>
2026-03-22 23:30:15 -0300

    me perdi aqui

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+double somar(double a, double b);
+double subtrair(double a, double b);
+double multiplicar(double a, double b);
+double dividir(double a, double b);
+
+int main() {
+    char operacao;
+    double num1, num2, resultado;
+
+    printf("Digite a operação (+, -, *, /): ");
+    scanf(" %c", &operacao);
+
+    printf("Digite o primeiro número: ");
+    scanf("%lf lf", %n1, %n2);
+}
